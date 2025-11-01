aa191fe438701c5e5bbc5aa897a92a46e876d672
Jen Junn <jenjunn@Kokomo.local>
2025-11-01 21:42:26 +0800

    added header file libraries, to check

---
+++ includes/minishell.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jenlee <jenlee@student.42kl.edu.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/01 21:34:29 by jenjunn           #+#    #+#             */
+/*   Updated: 2025/11/01 21:41:22 by jenlee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+#include "../Libft/libft.h"
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <termios.h>
+#include <term.h>
+
+#endif
