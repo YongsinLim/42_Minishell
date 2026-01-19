f8fff2f5de928b5d06bcef6345a14fec57c79abc
YongsinLim <micky.lys@gmail.com>
2026-01-19 16:35:07 +0800

    signal_set.c : delete duplicate init_signals_child

--- src/signals/signal_sets.c
+++ src/signals/signal_sets.c
@@ -6,7 +6,7 @@
 /*   By: yolim <yolim@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/01 22:09:35 by jenlee            #+#    #+#             */
-/*   Updated: 2025/12/31 11:48:17 by yolim            ###   ########.fr       */
+/*   Updated: 2026/01/19 16:34:09 by yolim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,6 @@ void	sigint_handler_prompt(int sig)
 	rl_replace_line("", 0);
 	rl_on_new_line();
 	rl_redisplay();
-}
-
-void	init_signals_child(void)
-{
-	signal(SIGINT, SIG_DFL);
-	signal(SIGQUIT, SIG_DFL);
 }
 
 void	sigint_handler_heredoc(int sig)
