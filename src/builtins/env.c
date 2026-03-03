59251211440cc9f3a04cd3ac07a40880a6842a82
ABCSoup <jenjunnlee20@gmail.com>
2026-03-03 18:58:34 +0800

    pwd and cd

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: jenlee <jenlee@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/11 18:06:59 by jenlee            #+#    #+#             */
-/*   Updated: 2026/03/03 18:53:09 by jenlee           ###   ########.fr       */
+/*   Updated: 2026/03/03 18:58:24 by jenlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,18 @@
 
 int ft_env(t_minishell *minishell)
 {
-    while (minishell->env_list)
+    t_env *current;
+
+    current = minishell->env_list;
+    while (current)
     {
-        // Only print if value exists (export VAR vs export VAR=value)
-        if (minishell->env_list->value)
+        if (current->value)
         {
-            ft_putstr_fd(minishell->env_list->key, 1);
+            ft_putstr_fd(current->key, 1);
             ft_putstr_fd("=", 1);
-            ft_putendl_fd(minishell->env_list->value, 1);
+            ft_putendl_fd(current->value, 1);
         }
-        minishell->env_list = minishell->env_list->next;
+        current = current->next;
     }
     return (SHELL_SUCCESS);
 }
